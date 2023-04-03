799b29cb342eb867692b1cce124743a75495f467
Salvatore <111086457+salvo081@users.noreply.github.com>
2023-04-03 12:10:26 +0200

    message array added

---
+++ script.js
@@ -0,0 +1,5 @@
+const msg = [
+    ['When you have a dream', 'Nothing is impossible', 'You make a choice', 'Success is not final'],  
+    ['you\'ve got to grab it', 'you\'ve got to hold on tight', 'don\'t let other people write your script'],
+    ['never let it go', 'do it', 'learn everything', 'to be successful', 'achive all goals']
+];
