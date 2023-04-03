0a22ceb5f65394d962313e91a15e564d3e841e33
Salvatore <111086457+salvo081@users.noreply.github.com>
2023-04-03 13:48:38 +0200

    message corrected

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const msg = [
     ['When you have a dream', 'Nothing is impossible', 'You make a choice', 'Success is not final'],  
     ['you\'ve got to grab it', 'you\'ve got to hold on tight', 'don\'t let other people write your script'],
-    ['never let it go', 'do it', 'learn everything', 'to be successful', 'achieve all goals']
+    ['never let it go', 'do it', 'learn everything', 'be successful', 'achieve all goals']
 ];
 
 const getRandomNumber = (maxValue) => Math.floor(Math.random() * maxValue);
